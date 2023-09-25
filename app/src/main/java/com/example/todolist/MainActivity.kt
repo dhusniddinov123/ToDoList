fa19fd9caee1fe7e1255ec92671ebfe428d5c661
DOSTONboy <89970820+dhusniddinov123@users.noreply.github.com>
2023-09-25 10:22:19 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.todo.screen.HomeFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
